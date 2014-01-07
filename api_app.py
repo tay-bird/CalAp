4fba1c0bc2bd1b8a761ac037a9561f1024a778a8
Taybird <taylor@taybird.com>
2014-01-06 22:09:20 -0700

    strip down api app

--- api_app.py
+++ api_app.py
@@ -1,30 +1,3 @@
-# -*- coding: utf-8 -*-
-#
-# Copyright (C) 2013 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""Command-line skeleton application for Calendar API.
-Usage:
-  $ python sample.py
-
-You can also get help on all the command-line flags the program understands
-by running:
-
-  $ python sample.py --help
-
-"""
-
 import argparse
 import httplib2
 import os
@@ -43,17 +16,10 @@ def main(argv=[]):
     formatter_class=argparse.RawDescriptionHelpFormatter,
     parents=[tools.argparser])
 
-
-  # CLIENT_SECRETS is name of a file containing the OAuth 2.0 information for this
-  # application, including client_id and client_secret. You can see the Client ID
-  # and Client secret on the APIs page in the Cloud Console:
   # <https://cloud.google.com/console#/project/151365153414/apiui>
   CLIENT_SECRETS = os.path.join(os.path.dirname(__file__), 'client_secrets.json')
       
   # Set up a Flow object to be used for authentication.
-  # Add one or more of the following scopes. PLEASE ONLY ADD THE SCOPES YOU
-  # NEED. For more information on using scopes please see
-  # <https://developers.google.com/+/best-practices>.
   FLOW = client.flow_from_clientsecrets(CLIENT_SECRETS,
     scope=[
     'https://www.googleapis.com/auth/calendar',
@@ -64,9 +30,7 @@ def main(argv=[]):
   # Parse the command-line flags.
   flags = parser.parse_args(argv[1:])
 
-  # If the credentials don't exist or are invalid run through the native client
-  # flow. The Storage object will ensure that if successful the good
-  # credentials will get written back to the file.
+  # If the credentials don't exist or are invalid run through the client flow.
   storage = file.Storage('sample.dat')
   credentials = storage.get()
   if credentials is None or credentials.invalid:
@@ -81,20 +45,3 @@ def main(argv=[]):
   service = discovery.build('calendar', 'v3', http=http)
 
   return service
-
-
-# For more information on the Calendar API you can visit:
-#
-#   https://developers.google.com/google-apps/calendar/firstapp
-#
-# For more information on the Calendar API Python library surface you
-# can visit:
-#
-#   https://developers.google.com/resources/api-libraries/documentation/calendar/v3/python/latest/
-#
-# For information on the Python Client Library visit:
-#
-#   https://developers.google.com/api-client-library/python/start/get_started
-if __name__ == '__main__':
-  main(sys.argv)
-  print sys.argv
